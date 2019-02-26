58064e54d43f26e26f36fc0c855fb275274a1a7b
Josh Cook <joshuacook0023@gmail.com>
2019-02-25 22:47:11 -0500

    doc: working through commenting the original code; passes: build, doc, check

--- R/new-scale.R
+++ R/new-scale.R
@@ -35,136 +35,155 @@
 #'
 #' @export
 new_scale <- function(new_aes) {
-  structure(ggplot2::standardise_aes_names(new_aes), class = "new_aes")
+    structure(ggplot2::standardise_aes_names(new_aes), class = "new_aes")
 }
 
 #' @export
 #' @rdname new_scale
 new_scale_fill <- function() {
-  new_scale("fill")
+    new_scale("fill")
 }
 
 #' @export
 #' @rdname new_scale
 new_scale_color <- function() {
-  new_scale("colour")
+    new_scale("colour")
 }
 
 #' @export
 #' @rdname new_scale
 new_scale_colour <- function() {
-  new_scale("colour")
+    new_scale("colour")
 }
 
 #' @export
 #' @importFrom ggplot2 ggplot_add
 ggplot_add.new_aes <- function(object, plot, object_name) {
-  plot$layers <- bump_aes_layers(plot$layers, new_aes = object)
-  plot$scales$scales <- bump_aes_scales(plot$scales$scales, new_aes = object)
-  plot$labels <- bump_aes_labels(plot$labels, new_aes = object)
-  plot
+    plot$layers <- bump_aes_layers(plot$layers, new_aes = object)
+    plot$scales$scales <- bump_aes_scales(plot$scales$scales, new_aes = object)
+    plot$labels <- bump_aes_labels(plot$labels, new_aes = object)
+    plot
 }
 
 bump_aes_layers <- function(layers, new_aes) {
-  lapply(layers, bump_aes_layer, new_aes = new_aes)
+    lapply(layers, bump_aes_layer, new_aes = new_aes)
 
 }
 
 bump_aes_layer <- function(layer, new_aes) {
-  original_aes <- new_aes
+    original_aes <- new_aes
 
-  old_aes <- names(layer$mapping)[remove_new(names(layer$mapping)) %in% new_aes]
-  new_aes <- paste0(old_aes, "_new")
-
-  old_geom <- layer$geom
-
-  old_setup <- old_geom$handle_na
-  new_setup <- function(self, data, params) {
-    colnames(data)[colnames(data) %in% new_aes] <- original_aes
-    old_setup(data, params)
-  }
-
-  new_geom <- ggplot2::ggproto(paste0("New", class(old_geom)[1]), old_geom,
-                               handle_na = new_setup)
-
-  new_geom$default_aes <- change_name(new_geom$default_aes, old_aes, new_aes)
-  new_geom$non_missing_aes <- change_name(new_geom$non_missing_aes, old_aes, new_aes)
-  new_geom$required_aes <- change_name(new_geom$required_aes, old_aes, new_aes)
-  new_geom$optional_aes <- change_name(new_geom$optional_aes, old_aes, new_aes)
-
-  layer$geom <- new_geom
-
-  old_stat <- layer$stat
-
-  old_setup2 <- old_stat$handle_na
-  new_setup <- function(self, data, params) {
-    colnames(data)[colnames(data) %in% new_aes] <- original_aes
-    old_setup2(data, params)
-  }
-
-  new_stat <- ggplot2::ggproto(paste0("New", class(old_stat)[1]), old_stat,
-                               handle_na = new_setup)
-
-  new_stat$default_aes <- change_name(new_stat$default_aes, old_aes, new_aes)
-  new_stat$non_missing_aes <- change_name(new_stat$non_missing_aes, old_aes, new_aes)
-  new_stat$required_aes <- change_name(new_stat$required_aes, old_aes, new_aes)
-  new_stat$optional_aes <- change_name(new_stat$optional_aes, old_aes, new_aes)
-
-  layer$stat <- new_stat
-
-  layer$mapping <- change_name(layer$mapping, old_aes, new_aes)
-  layer
-}
-
-bump_aes_scales <- function(scales, new_aes) {
-  lapply(scales, bump_aes_scale, new_aes = new_aes)
-}
-
-bump_aes_scale <- function(scale, new_aes) {
-  old_aes <- scale$aesthetics[remove_new(scale$aesthetics) %in% new_aes]
-  if (length(old_aes) != 0) {
+    old_aes <- names(layer$mapping)[remove_new(names(layer$mapping)) %in% new_aes]
     new_aes <- paste0(old_aes, "_new")
 
-    scale$aesthetics[scale$aesthetics %in% old_aes] <- new_aes
+    old_geom <- layer$geom
 
-    if (is.character(scale$guide)) {
-      scale$guide <- match.fun(paste("guide_", scale$guide, sep = ""))()
+    old_setup <- old_geom$handle_na
+    new_setup <- function(self, data, params) {
+        colnames(data)[colnames(data) %in% new_aes] <- original_aes
+        old_setup(data, params)
+   }
+
+    new_geom <- ggplot2::ggproto(paste0("New", class(old_geom)[1]), old_geom,
+                                 handle_na = new_setup)
+
+    new_geom$default_aes <- change_name(new_geom$default_aes, old_aes, new_aes)
+    new_geom$non_missing_aes <- change_name(new_geom$non_missing_aes, old_aes, new_aes)
+    new_geom$required_aes <- change_name(new_geom$required_aes, old_aes, new_aes)
+    new_geom$optional_aes <- change_name(new_geom$optional_aes, old_aes, new_aes)
+
+    layer$geom <- new_geom
+
+    old_stat <- layer$stat
+
+    old_setup2 <- old_stat$handle_na
+    new_setup <- function(self, data, params) {
+       colnames(data)[colnames(data) %in% new_aes] <- original_aes
+       old_setup2(data, params)
     }
-    scale$guide$available_aes[scale$guide$available_aes %in% old_aes] <- new_aes
-  }
-  scale
+
+    new_stat <- ggplot2::ggproto(paste0("New", class(old_stat)[1]), old_stat,
+                                 handle_na = new_setup)
+
+    new_stat$default_aes <- change_name(new_stat$default_aes, old_aes, new_aes)
+    new_stat$non_missing_aes <- change_name(new_stat$non_missing_aes, old_aes, new_aes)
+    new_stat$required_aes <- change_name(new_stat$required_aes, old_aes, new_aes)
+    new_stat$optional_aes <- change_name(new_stat$optional_aes, old_aes, new_aes)
+
+    layer$stat <- new_stat
+
+    layer$mapping <- change_name(layer$mapping, old_aes, new_aes)
+    layer
 }
 
+
+
+#### SCALES ####
+
+bump_aes_scales <- function(scales, new_aes) {
+    lapply(scales, bump_aes_scale, new_aes = new_aes)
+}
+
+
+bump_aes_scale <- function(scale, new_aes) {
+    # gets labels of current aes of `scale` of type `new_aes`
+    old_aes <- scale$aesthetics[remove_new(scale$aesthetics) %in% new_aes]
+    if (length(old_aes) != 0) {
+        # add another "_new" aes
+        new_aes <- paste0(old_aes, "_new")
+        # replace `old_aes` with `new_aes` with additional "_new" suffix
+        scale$aesthetics[scale$aesthetics %in% old_aes] <- new_aes
+
+        # I'm not sure what this does
+        if (is.character(scale$guide)) {
+            # scale$guide <- match.fun(paste("guide_", scale$guide, sep = ""))()
+            scale$guide <- match.fun(paste0("guide_", scale$guide))()
+        }
+
+        # adds "_new" suffix to `available_aes` attribute
+        scale$guide$available_aes[scale$guide$available_aes %in% old_aes] <- new_aes
+    }
+    scale
+}
+
+
+
+#### LABELS ####
+
+# adds another "_new" to all aes of type `new_aes`
 bump_aes_labels <- function(labels, new_aes) {
-  old_aes <-  names(labels)[remove_new(names(labels)) %in% new_aes]
-  new_aes <- paste0(old_aes, "_new")
-
-  names(labels)[names(labels) %in% old_aes] <- new_aes
-  labels
+    # labels of aes of type `new_aes` after removing all of the "_new"s
+    old_aes <-  names(labels)[remove_new(names(labels)) %in% new_aes]
+    # add another "_new" suffix to these labels
+    new_aes <- paste0(old_aes, "_new")
+    # replace those labels
+    names(labels)[names(labels) %in% old_aes] <- new_aes
+    labels
 }
 
-
+# of values in `list`, changes `old` --> `new`
 change_name <- function(list, old, new) {
-  UseMethod("change_name")
+    UseMethod("change_name")
 }
 
 change_name.character <- function(list, old, new) {
-  list[list %in% old] <- new
-  list
+    list[list %in% old] <- new
+    list
 }
 
 change_name.default <- function(list, old, new) {
-  nam <- names(list)
-  nam[nam %in% old] <- new
-  names(list) <- nam
-  list
+    nam <- names(list)
+    nam[nam %in% old] <- new
+    names(list) <- nam
+    list
 }
 
 change_name.NULL <- function(list, old, new) {
-  NULL
+    NULL
 }
 
-
+# removes the "_new" from "<txt>_new"
 remove_new <- function(aes) {
-  stringi::stri_replace_all(aes, "", regex = "(_new)*")
+    stringr::str_replace_all(aes, "_new", "")
+    # stringi::stri_replace_all(aes, "", regex = "(_new)*")
 }
